e5415d91b2078d85cceb6e03739494beaee3f57a
franck-lynn <lry_demry@163.com>
2021-12-11 12:42:34 +0800

    11

--- mongodb官方/基础/01.连接数据库.ts
+++ mongodb官方/基础/01.连接数据库.ts
@@ -1,18 +1,18 @@
 // https://docs.mongodb.com/drivers/node/current/quick-start/
 import {MongoClient} from "mongodb"
 // url 带上复制集, 数据库连接地址
-// const url = "mongodb://localhost:27017/replicaSet=my_repl"
+// const uri = "mongodb://localhost:27017/replicaSet=my_repl"
 // 也可以不带数据集
-const url = "mongodb://localhost:27017" 
-// 数据量连接对象
-const client = new MongoClient(url)
+const uri = "mongodb://localhost:27017" 
+// 数据库连接对象
+const client = new MongoClient(uri)
 
 const run = async () => {
     try {
         await client.connect() // 等待数据库连接
         const database = client.db("test") // 哪个数据库
-        const users = database.collection("actors") // 哪个集合
-        const query = {name: "奥黛丽·赫本"} // 查询哪一个文档
+        const users = database.collection("users") // 哪个集合
+        const query = {name: "franck-lynn"} // 查询哪一个文档
         const user = await users.findOne(query)
         console.log(user)
     } finally{
